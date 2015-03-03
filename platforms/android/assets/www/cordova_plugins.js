de1fe6173d4e07fe28bd24a1f9564e89f363d92c
Rodrigo <rlopez@rlm.com.mx>
2015-03-03 09:00:37 -0600

    Reducción

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -7,58 +7,12 @@ module.exports = [
             "cordova.InAppBrowser.open",
             "window.open"
         ]
-    },
-    {
-        "file": "plugins/com.kernix.pdfviewer/www/pdfviewer.js",
-        "id": "com.kernix.pdfviewer.PDFViewer",
-        "clobbers": [
-            "PDFViewer"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.device-motion/www/Acceleration.js",
-        "id": "org.apache.cordova.device-motion.Acceleration",
-        "clobbers": [
-            "Acceleration"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.device-motion/www/accelerometer.js",
-        "id": "org.apache.cordova.device-motion.accelerometer",
-        "clobbers": [
-            "navigator.accelerometer"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.device-orientation/www/CompassError.js",
-        "id": "org.apache.cordova.device-orientation.CompassError",
-        "clobbers": [
-            "CompassError"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.device-orientation/www/CompassHeading.js",
-        "id": "org.apache.cordova.device-orientation.CompassHeading",
-        "clobbers": [
-            "CompassHeading"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.device-orientation/www/compass.js",
-        "id": "org.apache.cordova.device-orientation.compass",
-        "clobbers": [
-            "navigator.compass"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.inappbrowser": "0.6.1-dev",
-    "com.kernix.pdfviewer": "1.1.0",
-    "org.apache.cordova.device-motion": "0.2.12-dev",
-    "org.apache.cordova.device-orientation": "0.3.12-dev",
-    "org.apache.cordova.geolocation": "0.3.13-dev"
+    "org.apache.cordova.inappbrowser": "0.6.1-dev"
 }
 // BOTTOM OF METADATA
 });
